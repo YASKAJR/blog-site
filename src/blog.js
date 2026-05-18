fe22217b9cfc3e14b2c15f3ef3dd9f1b85dd1725
YASKAJR <moyasin292@gmail.com>
2026-05-18 18:05:39 +0300

    Completed blog site lab

---
+++ src/blog.js
@@ -0,0 +1,34 @@
+const blog = {
+  name: "Mohamed's Blog",
+
+  image:
+    "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d",
+
+  about:
+    "This is my personal blog where I share coding experiences.",
+
+  posts: [
+    {
+      id: 1,
+      title: "Learning React",
+      date: "May 18, 2026",
+      preview: "React helps developers build modern websites."
+    },
+
+    {
+      id: 2,
+      title: "JavaScript Basics",
+      date: "May 15, 2026",
+      preview: "JavaScript is one of the most important web languages."
+    },
+
+    {
+      id: 3,
+      title: "Frontend Development",
+      date: "May 10, 2026",
+      preview: "Frontend development focuses on user interfaces."
+    }
+  ]
+};
+
+export default blog;
