f425f31b513fb1258e7ec9082010be3ad349eb6e
YASKAJR <moyasin292@gmail.com>
2026-05-20 22:37:32 +0300

    Completed blog site lab

---
+++ src/data/blog.js
@@ -0,0 +1,30 @@
+const blog = {
+  name: "Mohamed's Blog",
+
+  about: "I enjoy coding and learning React.",
+
+  articles: [
+    {
+      id: 1,
+      title: "Learning React",
+      date: "May 18, 2026",
+      preview: "React is a JavaScript library for building UIs."
+    },
+
+    {
+      id: 2,
+      title: "Understanding Props",
+      date: "May 17, 2026",
+      preview: "Props help pass data between components."
+    },
+
+    {
+      id: 3,
+      title: "Why Components Matter",
+      date: "May 16, 2026",
+      preview: "Components make apps reusable and organized."
+    }
+  ]
+};
+
+export default blog;
